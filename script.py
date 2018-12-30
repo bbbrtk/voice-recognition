f6254f5f22f9e17a7c9d2964123382626457c336
bbbrtk <sobkowiak.bart@gmail.com>
2018-12-30 20:05:22 +0100

    add fft and decimate

--- script.py
+++ script.py
@@ -6,65 +6,9 @@ from scipy import *
 from ipywidgets import *
 import math as mt 
 import matplotlib.pyplot as plt
+import warnings
 
-def myFunction(A=1, LP=1, w=50, f=2.0):
-    
-#   w = 2678400
-    T = 1.0/f        
-    # Okres sygnaku [s] (jak dkugo trwa jeden przebieg sinusa)
-
-#     TW = 1.0/w 
-    TW = 2678400
-    w = 1.0/TW
-    # Okres probkowania [s] (co ile sekund pobieramy
-
-    t = np.arange(0, LP*T, TW)
-    # generujemy momenty, w ktorych pobieramy proki
-
-    #signal = array=[float(x) for x in open('spots.txt').read().split()] 
-    w, signal = scipy_read(sys.argv[1])
-    signal = [s[0] for s in signal]            
-    # funkcja sprobkowana
-    
-    n = len(signal)
-    
-    
-    fig = plt.figure(figsize=(15, 6), dpi=80)   
-    ax = fig.add_subplot(121)
-    ## --- POMOCNICZY SYGNAL
-    base_t = np.arange(0, n, 1)
-    base_signal = signal
-    ax.plot(base_t, base_signal, linestyle='-', color='red')
-    ax.set_ylim([min(base_signal), max(base_signal)])
-    ## --- 
-#     ax.plot(t, signal, 'o')
-
-    signal1 = fft(signal)
-    # sygnal w dziedzinie czestotliwosci 
-    signal1 = abs(signal1)        
-    # modul sygnalu
-
-    freqs = np.arange(0,w,w/n)
-
-
-    ax = fig.add_subplot(122)
-    ymax = max(signal1)
-    if (ymax > 3.0):
-        ax.set_ylim([0.0,ymax])
-    plt.stem(freqs, signal1, '-*')
-    ax.set_xlabel('[Hz]')
-    plt.show()
-    
-    tempx = (argmax(signal1[1:])+1) * w/n
-    tempx = 1.0/tempx / 2678400.0 / 12.0
-    print(tempx)
-    
-myFunction()
-
-
-print("_start")
-w, signal = scipy_read(sys.argv[1])
-signal = [s[0] for s in signal]
+import soundfile as sf 
 
 # show signal on 2 plots
 # make n copies of fft signal
@@ -74,5 +18,56 @@ signal = [s[0] for s in signal]
 # decide if male or female
 
 
-answer = "K"
-print(answer)
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+
+# w, signal = scipy_read(sys.argv[1])
+signal, w = sf.read(sys.argv[1])
+
+if len(signal.shape) > 1:
+    signal = [s[0] for s in signal]
+
+#spectrum = signal[::10]
+spectrum = signal 
+
+fig = plt.figure(figsize=(15, 6), dpi=80)   
+plotnum = 811
+ax = fig.add_subplot(plotnum)
+
+ax.plot(spectrum, linestyle='-', color='red')
+ax.set_ylim([min(spectrum), max(spectrum)])
+# --- 
+
+signal = np.fft.fft(spectrum)
+ffts = []
+for i in range (5):
+    signal = decimate(signal, 2, zero_phase=False)
+    ffts.append(signal)
+    #signal = signal[signal>0]
+    #signal = abs(signal)
+
+    
+    plotnum += 1
+    ax = fig.add_subplot(plotnum)  
+    ax.set_xlim([0, 1000])
+
+    plt.plot(signal)
+
+    #multiply_signal *= signal
+    #np.multiply(multiply_signal, signal)
+sum = ffts[0][:1000]
+for j in range(4):
+    sum  *= ffts[j+1][:1000]
+
+ 
+plotnum += 1
+ax = fig.add_subplot(plotnum)
+ax.set_xlim([0, 1000])
+plt.plot(sum)
+
+j = list(sum).index(max(sum))
+print(j)
+plt.show()
+
+
